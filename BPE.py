703a13f250564eb20954baf6c6ec15da63344d8a
GSatya147 <gvvsn147@gmail.com>
2026-05-01 22:50:21 +0100

    BPE: working implementation

--- BPE.py
+++ BPE.py
@@ -2,10 +2,41 @@
 BPE Implementation - Byte Pair Encoding
 - Compressing data for much shorter lengths of contexts and shorter vocabulary table
 
-process:
+Process:
 Step-1: Iterate and check for most occuring byte pairs in a sequence
 Step-2: Replace the most occured byte pairs with a new token
-Step-3: Recursicely repeat the process until no byte pairs are left to be replaced
+Step-3: Recursively repeat the process until no byte pairs are left to be replaced
+
+Working:
+Step-1: Example_text = "aaabdaaabac"
+        We have length '11' sequence with a vocabulary length '4'
+        vocabulary = {a, b, c, d}
+
+Step-2: Most occuring byte pair = 'aa'
+        Replace it with 'Z' (a new token)
+
+        we get = "ZabdZabac"
+        replacement table: 'Z' = 'aa'
+
+Step-3: Most occuring byte pair = 'ab'
+        Replace it with 'Y' (a new token)
+
+        we get = "ZYdZYac"
+        replacement table:  'Y' = 'ab'
+                            'Z' = 'aa'
+
+Step-4: Now recursively applying BPE
+        Most occuring byte pair = 'ZY'
+        Replace it with 'X' (a new token)
+
+        we get = "XdXac"
+        replacement table:  'X' = 'ZY'
+                            'Y' = 'ab'
+                            'Z' = 'aa'
+
+Step-5: Compressed_text = "XdXac"
+        Now we have length '%' sequence with a vocabulary length '3'
+        vocabulary = {X, Y, Z}
 
 """
 
